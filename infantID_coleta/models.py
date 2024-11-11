faaf104a5e009c513449b383220f72d1f2f5aeec
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2024-11-11 16:44:54 -0300

    starting models

--- infantID_coleta/models.py
+++ infantID_coleta/models.py
@@ -1,3 +1,106 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Agenda(models.Model):
+    id_agenda = models.IntegerField(primary_key=True)
+    data_agenda = models.DateField(blank=True, null=True)
+    tipo_rc = models.IntegerField(blank=True, null=True)
+    id_cadastro = models.ForeignKey('Cadastro', models.DO_NOTHING, db_column='id_cadastro', blank=True, null=True)
+    id_responsavel = models.ForeignKey('Responsvel', models.DO_NOTHING, db_column='id_responsavel', blank=True, null=True)
+    id_recoleta = models.ForeignKey('Recoleta', models.DO_NOTHING, db_column='id_recoleta', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'agenda'
+
+
+
+class Cadastro(models.Model):
+    id_cadastro = models.CharField(primary_key=True, max_length=10)
+    peso = models.FloatField(blank=True, null=True)
+    altura = models.IntegerField(blank=True, null=True)
+    semanas_gestacao = models.IntegerField(blank=True, null=True)
+    sexo = models.CharField(max_length=1, blank=True, null=True)
+    scanner = models.CharField(max_length=20, blank=True, null=True)
+    data_coleta = models.DateField(blank=True, null=True)
+    observacao = models.CharField(max_length=100, blank=True, null=True)
+    nome_hospital = models.ForeignKey('Hospital', models.DO_NOTHING, db_column='nome_hospital', blank=True, null=True)
+    nome_coletista = models.ForeignKey('Coletista', models.DO_NOTHING, db_column='nome_coletista', blank=True, null=True)
+    id_responsavel = models.ForeignKey('Responsvel', models.DO_NOTHING, db_column='id_responsavel', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cadastro'
+
+
+class Coletista(models.Model):
+    nome = models.CharField(primary_key=True, max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'coletista'
+
+
+class Desvinculo(models.Model):
+    id_desvinculo = models.IntegerField(primary_key=True)
+    data_desvinculo = models.DateField(blank=True, null=True)
+    motivo = models.CharField(max_length=50, blank=True, null=True)
+    id_cadastro = models.ForeignKey(Cadastro, models.DO_NOTHING, db_column='id_cadastro', blank=True, null=True)
+    idresponsavel = models.ForeignKey('Responsvel', models.DO_NOTHING, db_column='idresponsável', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'desvinculo'
+
+
+
+class Hospital(models.Model):
+    nome = models.CharField(primary_key=True, max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'hospital'
+
+
+class Materiais(models.Model):
+    id_materiais = models.IntegerField(primary_key=True)
+    carteirinha = models.BooleanField(blank=True, null=True)
+    mordedor = models.BooleanField(blank=True, null=True)
+    gorro = models.BooleanField(blank=True, null=True)
+    id_cadastro = models.ForeignKey(Cadastro, models.DO_NOTHING, db_column='id_cadastro', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'materiais'
+
+
+class Recoleta(models.Model):
+    id_recoleta = models.IntegerField(primary_key=True)
+    scanner = models.CharField(max_length=20, blank=True, null=True)
+    data_recoleta = models.DateField(blank=True, null=True)
+    idcadastro = models.ForeignKey(Cadastro, models.DO_NOTHING, db_column='idcadastro', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'recoleta'
+
+
+class Responsvel(models.Model):
+    id_responsavel = models.CharField(primary_key=True, max_length=10)
+    nome_responsavel = models.CharField(max_length=45, blank=True, null=True)
+    telefone_responsavel = models.BigIntegerField(blank=True, null=True)
+    endereco_cadastro = models.CharField(max_length=45, blank=True, null=True)
+    bairro_cadastro = models.CharField(max_length=20, blank=True, null=True)
+    endereco_atual = models.CharField(max_length=45, blank=True, null=True)
+    bairro_atual = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'responsável'
--- setup/settings.py
+++ setup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': 'coletas_infantid',
+        'USER': 'admin',
+        'PASSWORD': "pD9LgngnXR9vNoDAc34CH8Y0d099DtZx",
+        'HOST': 'dpg-csp57rm8ii6s73c8mt3g-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
