7d6ea7701aa46f6075664ad1ff3a71a4bb66fb66
LuizCenci <155825996+LuizCenci@users.noreply.github.com>
2024-11-29 17:08:32 -0300

    new class for forms

--- infantID_coleta/forms.py
+++ infantID_coleta/forms.py
@@ -12,6 +12,13 @@ class cadastro_responsavel(forms.ModelForm):
             'endereco_cadastro':'Endereço',
             'bairro_cadastro':'Bairro',
         }
+        widgets = {
+            'id_responsavel':forms.TextInput(attrs={'class':'form-control'}),
+            'nome_responsavel':forms.TextInput(attrs={'class':'form-control'}),
+            'telefone_responsavel':forms.TextInput(attrs={'class':'form-control'}),
+            'endereco_cadastro':forms.TextInput(attrs={'class':'form-control'}),
+            'bairro_cadastro':forms.TextInput(attrs={'class':'form-control'}),
+        }
         
 class cadastro_coleta(forms.ModelForm):
     class Meta:
@@ -30,13 +37,34 @@ class cadastro_coleta(forms.ModelForm):
             'nome_coletista':'Selecionar Coletista',
             'id_responsavel':'Selecionar Responsável'
         }
-
+        widgets = {
+            'id_cadastro':forms.TextInput(attrs={'class':'form-control'}),
+            'peso':forms.TextInput(attrs={'class':'form-control'}),
+            'altura':forms.TextInput(attrs={'class':'form-control'}),
+            'semanas_gestacao':forms.TextInput(attrs={'class':'form-control'}),
+            'sexo':forms.Select(attrs={'class':'form-control'}),
+            'scanner':forms.TextInput(attrs={'class':'form-control'}),
+            'data_coleta':forms.DateInput(
+                format = '%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'form-control'
+                }
+            ),
+            'observacao':forms.Textarea(attrs={'class':'form-control'}),
+            'nome_hospital':forms.Select(attrs={'class':'form-control'}),
+            'nome_coletista':forms.Select(attrs={'class':'form-control'}),
+            'id_responsavel':forms.Select(attrs={'class':'form-control'}),
+        }
 class cadastro_coletista(forms.ModelForm):
     class Meta:
         model = Coletista
         exclude = ['']
         labels = {
             'nome':'Nome'
+        }
+        widgets = {
+            'nome':forms.TextInput(attrs={'class':'form-control'}),
         }
 
 class cadastro_hospital(forms.ModelForm):
@@ -45,6 +73,9 @@ class cadastro_hospital(forms.ModelForm):
         exclude = ['']
         labels = {
             'nome':'Nome'
+        }
+        widgets = {
+            'nome':forms.TextInput(attrs={'class':'form-control'}),
         }
 
 class cadastro_Agenda(forms.ModelForm):
@@ -58,6 +89,19 @@ class cadastro_Agenda(forms.ModelForm):
             'id_responsavel':'Selecionar Responsável',
             'id_recoleta':'Selecionar Recoleta',
         }
+        widgets = {
+            'data_agenda':forms.DateInput(
+                format = '%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'form-control'
+                }
+            ),
+            'tipo_rc':forms.Select(attrs={'class':'form-control'}),
+            'id_cadastro':forms.Select(attrs={'class':'form-control'}),
+            'id_responsavel':forms.Select(attrs={'class':'form-control'}),
+            'id_recoleta':forms.Select(attrs={'class':'form-control'}),
+        }
 
 class cadastro_Recoleta(forms.ModelForm):
     class Meta:
@@ -67,4 +111,15 @@ class cadastro_Recoleta(forms.ModelForm):
             'scanner':'Scanner',
             'data_recoleta':'Data da Recoleta',
             'idcadastro':'ID Cadastrado',
+        }
+        widgets ={
+            'scanner':forms.TextInput(attrs={'class':'form-control'}),
+            'data_recoleta':forms.DateInput(
+                format = '%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'form-control'
+                }
+            ),
+            'idcadastro':forms.Select(attrs={'class':'form-control'})
         }
