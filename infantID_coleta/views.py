879d11047594573664c6cf051aa8f9c277f3d704
LuizCenci <155825996+LuizCenci@users.noreply.github.com>
2024-12-10 11:53:57 -0300

    correct form action on cadastro

--- infantID_coleta/views.py
+++ infantID_coleta/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from infantID_coleta.models import *
 from infantID_coleta.forms import *
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'partials/index.html')
+    return render(request, 'pages/index.html')
 
 
 def novo_responsavel(request):
@@ -14,6 +15,7 @@ def novo_responsavel(request):
         responsavel = cadastro_responsavel(request.POST)
         if responsavel.is_valid():
             responsavel.save()
+            messages.success(request, 'Responsável adicionado com sucesso!')
             return redirect('formulario:index')
 
     context = {'form':responsavel}
@@ -39,19 +41,17 @@ def novo_cadastro_create(request):
 
     form = cadastro_coleta(POST)
     if form.is_valid():
-        infos = form.save(commit=False)  
+        infos = form.save(commit=False)
 
         n_filhos = Cadastro.objects.filter(id_responsavel=infos.id_responsavel).count()
         infos.id_cadastro = f'{infos.id_responsavel.id_responsavel}_0{n_filhos + 1}'
         infos.save()
-
         del request.session['cadastro_form_data']
 
-        #messages.success(request, 'Cadastro criado com sucesso!')
+        messages.success(request, 'Cadastro criado com sucesso!')
 
         return redirect('formulario:index') 
-
-    return redirect('novo_cadastro_view')  
+    return redirect('formulario:novo_cadastro_view')    
 
 
 
@@ -62,6 +62,7 @@ def novo_coletista(request):
         coletista = cadastro_coletista(request.POST)
         if coletista.is_valid():
             coletista.save()
+            messages.success(request, 'Coletista adicionado com sucesso!')
             return redirect('formulario:index')
 
     context = {'form':coletista}
@@ -74,6 +75,7 @@ def novo_hospital(request):
         hospital = cadastro_hospital(request.POST)
         if hospital.is_valid():
             hospital.save()
+            messages.success(request, 'Hospital adicionado com sucesso!')
             return redirect('formulario:index')
 
     context = {'form':hospital}
@@ -86,6 +88,7 @@ def nova_agenda(request):
         agenda = cadastro_Agenda(request.POST)
         if agenda.is_valid():
             agenda.save()
+            messages.success(request, 'Agenda adicionada com sucesso!')
             return redirect('formulario:index')
 
     context = {'form':agenda}
@@ -97,6 +100,7 @@ def nova_recoleta(request):
         recoleta = cadastro_Recoleta(request.POST)
         if recoleta.is_valid():
             recoleta.save()
+            messages.success(request, 'Recoleta adicionada com sucesso!')
             return redirect('formulario:index')
 
     context = {'form':recoleta}
